6eb295fa2b165c9ef7769a0e436cf1b6fbab4134
xhevdet kryeziu <xhevdet.kryeziu@kujtesa.com>
2021-10-22 16:30:16 +0200

    first commit

---
+++ src/Categories.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Categories = () => {
+	return (
+		<div>
+			<button>shakes</button>
+			<button>breakfast</button>
+			<button>lunch</button>
+		</div>
+	);
+};
+
+export default Categories;
